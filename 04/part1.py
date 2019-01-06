f4cacbc30a3f72b496a952ea1bd895d80796d6e6
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2019-01-05 22:57:39 -0500

    Refactor get_minutes_slep_methods

--- 04/part1.py
+++ 04/part1.py
@@ -5,12 +5,6 @@ import datetime
 sys.path.insert(0, '../util')
 from utils import multiline_input
 
-data = multiline_input()
-
-schedule = data.split('\n')
-schedule.sort()
-sleep_log = {}
-
 def split_by_day(schedule):
     day_log = []
     for line in schedule:
@@ -20,8 +14,6 @@ def split_by_day(schedule):
             day_log = [line]
         else:
             day_log.append(line)
-        
-daily_schedule = list(split_by_day(schedule))
 
 def initialize_sleep_log(data, sleep_log):
     for event in data:
@@ -36,19 +28,23 @@ def get_id(line):
 
 def get_times_falling_asleep(day):
     times_falling_asleep = []
+    date_format = '%H:%M'
     for event in day:
         if "falls asleep" in event:
             match = re.search(r' (\d+:\d+)]', event)
             time = match.group(1)
+            time = datetime.datetime.strptime(time, date_format)
             times_falling_asleep.append(time)
     return times_falling_asleep
 
 def get_times_waking_up(day):
     times_waking_up = []
+    date_format = '%H:%M'
     for event in day:
         if "wakes up" in event:
             match = re.search(r' (\d+:\d+)]', event)
             time = match.group(1)
+            time = datetime.datetime.strptime(time, date_format)
             times_waking_up.append(time)
     return times_waking_up
 
@@ -56,33 +52,31 @@ def get_minutes_slept(day):
     minutes_slept = 0
     times_falling_asleep = get_times_falling_asleep(day)
     times_waking_up = get_times_waking_up(day)
-    date_format = '%H:%M'
     for time_falling_asleep, time_waking_up in zip(times_falling_asleep, times_waking_up):
-        time_difference = datetime.datetime.strptime(time_waking_up, date_format) - datetime.datetime.strptime(time_falling_asleep, date_format)
+        time_difference = time_waking_up  - time_falling_asleep
         difference_in_minutes = int(time_difference.total_seconds()/60)
         minutes_slept += difference_in_minutes
     return minutes_slept
 
-initialize_sleep_log(schedule, sleep_log)
+def fill_log(daily_schedule, sleep_log):
+    for day in daily_schedule:
+        line_with_id = day[0]
+        guard_id = get_id(line_with_id)
+        a = get_minutes_slept(day)
+        sleep_log[guard_id] += get_minutes_slept(day)
+    return sleep_log
 
-for day in daily_schedule:
-    line_with_id = day[0]
-    guard_id = get_id(line_with_id)
-    a = get_minutes_slept(day)
-    sleep_log[guard_id] += get_minutes_slept(day)
-print(sleep_log)
+def find_biggest_sleeper(sleep_log):
+    return max(sleep_log, key= sleep_log.get)
 
-"""
-i = 0
-key = ""
-format = '%H:%M'
-
-while i != len(schedule):
-    if ("#" in schedule[i]):
-        match = re.search(r'#(\d+) ')
-        key = match.group(1)
-    elif ("falls asleep" in schedule[i]):
-        match = re.search(r' (\d+)]')
-        minutes_slept[key] += int(match)
-    i += 1
-"""
+if __name__ == "__main__":
+    data = multiline_input()
+    sleep_log = {}
+    schedule = data.split('\n')
+    schedule.sort()
+    daily_schedule = list(split_by_day(schedule))
+    initialize_sleep_log(schedule, sleep_log)
+    fill_log(daily_schedule, sleep_log)
+    biggest_sleeper = find_biggest_sleeper(sleep_log)
+    print(sleep_log)
+    print(biggest_sleeper)
