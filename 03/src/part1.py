e94fd6ef45dcf727ea793e5f99c5452d98284890
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2019-01-09 11:34:20 -0500

    Fix day 3 part 1 solution. Solution now works.

--- 03/src/part1.py
+++ 03/src/part1.py
@@ -6,14 +6,14 @@ with open("../input/input.txt", "r") as input:
 class Claim():
     def __init__(self, claim):
         self.claim = claim
-        self.width_offset = self.get_digits_group(claim, 0)
-        self.height_offset = self.get_digits_group(claim, 1)
-        self.width = self.get_digits_group(claim, 2)
-        self.height = self.get_digits_group(claim, 3)
+        self.width_offset = self.get_digits_group(claim, 1)
+        self.height_offset = self.get_digits_group(claim, 2)
+        self.width = self.get_digits_group(claim, 3)
+        self.height = self.get_digits_group(claim, 4)
     
     @staticmethod
     def get_digits_group(line, i):
-        matches = re.findall(r'\d\d+', line)
+        matches = re.findall(r'\d+', line)
         digits = int(matches[i])
         return digits
 
@@ -35,7 +35,6 @@ class Fabric():
     
     def add_claims(self):
         claims = (Claim(row) for row in self.data)
-        i, y = 0, 0
         for claim in claims:
             self.fill_rectangle(claim)
 
@@ -46,9 +45,14 @@ class Fabric():
         y_counter = claim.width
         while i_counter != 0:
             while y_counter != 0:
-                self.fabric[i+i_counter-1][y+y_counter-1] += 1
+                try:
+                    self.fabric[i+i_counter-1][y+y_counter-1] += 1
+                except (IndexError):
+                    print("claim ",claim.claim, " requests a square inch out of the fabric (position:",i+i_counter-1, y+y_counter,")." )
+                    raise IndexError
                 y_counter -= 1
             i_counter -= 1
+            y_counter = claim.width
 
     def solve_part_1(self):
         counter = 0
@@ -64,14 +68,15 @@ class Fabric():
     
 
 if __name__ == "__main__":
-    a = Fabric(10, data)
+    a = Fabric(1000, data)
     b = Claim("#1 @ 146,196: 19x14")
-    assert(len(a.fabric) == 10)
-    assert(len(a.fabric[0]) == 10)
+    assert(len(a.fabric) == 1000)
+    assert(len(a.fabric[0]) == 1000)
+    """
     for row in a.fabric:
         for sqr_inch in row:
             assert(sqr_inch == 0)
-
+    """
     assert(b.width_offset == 146)
     assert(b.height_offset == 196)
     assert(b.width == 19)
