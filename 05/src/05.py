27b80dda4972fe6b5456f9314da58cadca627ca8
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2019-01-09 22:56:00 -0500

    Implement imperfect solution

--- 05/src/05.py
+++ 05/src/05.py
@@ -9,11 +9,22 @@ class Polymer():
         self.units = data
 
     def react(self):
-        matches = re.findall(r'([A-Z][a-z]|[a-z][A-Z]', self.units)
-        if matches:
-            for match in matches:
-                self.units = self.units.replace(match, "")
+        iterator = len(self.units) - 2
+        has_reaction = False
+        reactions = []
+        for l in range(0,iterator):
+            reaction_on_l = self.units[l] == self.units[l+1].upper() or self.units[l] == self.units[l+1].lower()
+            if (reaction_on_l):
+                units_involved = self.units[l] + self.units[l+1]
+                has_reaction = True
+                reactions.append(units_involved)
+        if has_reaction:
+            self.remove_reactions(reactions)
             self.react()
+
+    def remove_reactions(self, reactions):
+        for reaction in reactions:
+            self.units = self.units.replace(reaction, "")
 
     def get_units_count(self):
         return len(self.units)
@@ -21,13 +32,15 @@ class Polymer():
     def solve_part_1(self):
         self.react()
         return "The answer for part 1 is " + str(self.get_units_count()) + "."
+    
 
 if __name__ == "__main__":
     a = Polymer("dabAcCaCBAcCcaDA")
     assert(a.get_units_count() == 16)
     a.react()
-    print("units: ", a.units)
     assert(a.units == "dabCBAcaDA")
     assert(a.get_units_count() == 10)
     b = Polymer(data)
+    beginning = b.units
     print(b.solve_part_1())
+    print(b.units)
