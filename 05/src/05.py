7b69ea2d368f2951319a357eea11616b1c9a6dcc
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2019-01-09 21:57:40 -0500

    Refactor solution.

--- 05/src/05.py
+++ 05/src/05.py
@@ -4,15 +4,12 @@ import string
 with open("../input/input.txt", "r") as input:
     data = input.read()
 
-with open("../input/test.txt", "r") as input:
-    test_data = input.read()
-
 class Polymer():
     def __init__(self, data):
         self.units = data
 
     def react(self):
-        matches = re.findall(r'[A-Z][a-z]|[a-z][A-Z]', self.units)
+        matches = re.findall(r'([A-Z][a-z]|[a-z][A-Z]', self.units)
         if matches:
             for match in matches:
                 self.units = self.units.replace(match, "")
@@ -32,9 +29,5 @@ if __name__ == "__main__":
     print("units: ", a.units)
     assert(a.units == "dabCBAcaDA")
     assert(a.get_units_count() == 10)
-    c = Polymer(test_data)
-    c.react()
-    assert(c.units == "ccc")
-    assert(c.get_units_count())
     b = Polymer(data)
     print(b.solve_part_1())
