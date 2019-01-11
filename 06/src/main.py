9c5b4b5efd659dc87881facfea51d4b7230eab7c
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2019-01-11 13:41:45 -0500

    Add create and populate method in Grid class in Day 06 part 1.

--- 06/src/main.py
+++ 06/src/main.py
@@ -7,19 +7,30 @@ with open("../input/input.txt", "r") as input:
     data = input.read()
 
 class Coordinate():
+    new_id = 0
     def __init__(self, str_version):
         self.str_version = str_version
         self.x = utils.get_digits_group(self.str_version, 0)
         self.y = utils.get_digits_group(self.str_version, 1)
-        
+        Coordinate.new_id += 1
+        self.id = Coordinate.new_id
 
 class Grid():
     def __init__(self, coordinates_list):
         self.coordinates = [Coordinate(line) for line in coordinates_list]
         self.height = self.find_format()[0]
         self.width = self.find_format()[1]
-        
+        self.grid = self.create()
+
     def find_format(self):
-        height = max([coordinate.x for coordinate in self.coordinates])
-        width = max([coordinate.y for coordinate in self.coordinates])
+        height = max([coordinate.x for coordinate in self.coordinates])+1
+        width = max([coordinate.y for coordinate in self.coordinates])+1
         return (height, width)
+
+    def create(self):
+        return [["" for y in range(0, self.width)] for x in range(0, self.height)]
+
+    def populate(self):
+        for coordinate in self.coordinates:
+            self.grid[coordinate.x][coordinate.y] = str(coordinate.id)
+
--- 06/test/tests.py
+++ 06/test/tests.py
@@ -11,19 +11,26 @@ class CoordinateTestCase(unittest.TestCase):
     def test_init(self):
         self.assertEqual(self.coordinate.x, 287)
         self.assertEqual(self.coordinate.y, 68)
+        self.assertEqual(self.coordinate.id, 1)
 
 
 class GridTestCase(unittest.TestCase):
     def setUp(self):
-        coordinates_list = ["1, 1","2, 2", "2, 1", "1, 3"]
+        coordinates_list = ["0, 0","1, 1", "1, 0", "0, 2"]
         self.test_grid = Grid(coordinates_list)
     
     def test_init(self):
-        self.assertEqual(self.test_grid.width, 2)
-        self.assertEqual(self.test_grid.height, 3)
-        self.assertEqual(self.test_grid.grid, [["A", "", "D"],
-                                                   ["C", "B", ""],
-                                                   ["", "", ""]])
+        self.assertEqual(self.test_grid.height, 2)
+        self.assertEqual(self.test_grid.width, 3)
+
+    def test_create(self):
+        self.assertEqual(self.test_grid.grid, [["", "", ""],
+                                                    ["", "", ""]])
+    
+    def test_populate(self):
+        self.test_grid.populate()
+        self.assertEqual(self.test_grid.grid, [["10", "", "13"],
+                                                   ["12", "11", ""]])
 
 
 if __name__ == "__main__":
